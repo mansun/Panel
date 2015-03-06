9611d46c4764abe4a18404bda66b5d7a59534294
Enrique_W8 <Enrique_W8@EPV_W8.localdomain>
2015-03-06 19:26:20 +0100

    Hola

---
+++ Panel/modulos/rol/consulta.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<h3>Roles</h3>
+<a href="">Nuevo</a>
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Nombre</th>
+			<th>Tipo</th>
+			<th>Botones</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>admin</td>
+			<td>1</td>
+			<td><a href="">Editar</a><a href="">Borrar</a></td>
+		</tr>
+	</tbody>
+</table>
---
+++ Panel/scripts/inserciones-iniciales.sql
@@ -0,0 +1,3 @@
+INSERT INTO tipo_rol(tipoRolID, tipoRolNom) VALUES (1,'admin');
+INSERT INTO tipo_rol(tipoRolID, tipoRolNom) VALUES (2,'lector');
+INSERT INTO tipo_rol(tipoRolID, tipoRolNom) VALUES (3,'escritor');
